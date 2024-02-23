5a50c7c4c955cd7127738a62d1eaa7b35be6c144
happygolucky <76201487+happygoIucky@users.noreply.github.com>
2024-02-23 17:00:52 +0800

    Update provider.tf

--- provider.tf
+++ provider.tf
@@ -1,14 +1,17 @@
-terraform {
-  required_providers {
-    alicloud = {
-      source = "aliyun/alicloud"
-      version = "1.202.0"
-    }
-  }
+resource "alicloud_vpc" "main" {
+  # VPC name
+  name       = "alicloud"
+  # CIDR block of the VPC
+  cidr_block = "10.1.0.0/21"
 }
 
-provider "alicloud" {
-  	region  = "ap-southeast-1"
+resource "alicloud_vswitch" "main" {
+  # VPC ID
+  vpc_id            = alicloud_vpc.main.id
+  # CIDR block of the VSwitch
+  cidr_block        = "10.1.0.0/24"
+  # Zone
+  availability_zone = "ap-southeast-1a"
+  # Dependent resource (this dependent resource will be created first)
+  depends_on = [alicloud_vpc.main]
 }
-
-
