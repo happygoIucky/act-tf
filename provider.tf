fa523355c9a8e8a5fd3e8f134b5da1edd8476940
jawn-lim <junwen89@hotmail.com>
2024-02-23 23:11:19 +0800

    wqe

--- provider.tf
+++ provider.tf
@@ -11,7 +11,7 @@ provider "alicloud" {
   	region  = var.region
 #    shared_credentials_file = "/Users/Jawn Lim/.aliyun/config.json"
 #    profile                 = "aliprof"
-#    configuration_source = "terraform-provider-alicloud/examples/vpc"
+    configuration_source = "terraform-provider-alicloud/examples/vpc"
 
 }
 
