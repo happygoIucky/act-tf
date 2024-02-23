510b64d76854ab840d6c56d7587e2b67153dfd60
jawn-lim <junwen89@hotmail.com>
2024-02-23 21:22:25 +0800

    dd

--- provider.tf
+++ provider.tf
@@ -8,14 +8,14 @@ terraform {
 }
 
 provider "alicloud" {
-  	#region  = "ap-southeast-1"
-    #shared_credentials_file = "/Users/Jawn Lim/.aliyun/config.json"
-    #profile                 = "aliprof"
+  	region  = "ap-southeast-1"
+    shared_credentials_file = "/Users/Jawn Lim/.aliyun/config.json"
+    profile                 = "aliprof"
     configuration_source = "terraform-provider-alicloud/examples/vpc"
 
 }
 
 resource "alicloud_vpc" "vpc" {
-  name       = "tf_test_jl"
+  vpc_name   = "tf_test_jl"
   cidr_block = "172.16.0.0/12"
 }
