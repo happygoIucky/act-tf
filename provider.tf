f6b41bf4ddb147564d120188875376a674e2b2bd
happygolucky <76201487+happygoIucky@users.noreply.github.com>
2024-02-23 17:09:59 +0800

    Update provider.tf

--- provider.tf
+++ provider.tf
@@ -1,10 +1,5 @@
-terraform {
-  required_providers {
-    alicloud = {
-      source = "aliyun/alicloud"
-      version = "1.202.0"
-    }
-  }
+provider "alicloud" {
+  	region  = ap-southeast-1
 }
 
 resource "alicloud_vpc" "main" {
